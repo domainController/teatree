9b57a62e1b4f44052dd856ce7849a267e3d1a24c
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-02 19:07:40 +0000

    Fizzbuzz avec Robert Wallis dont le tuto s'averait etre bancal cf commentaire video https://youtu.be/j05lxYDk_8oqwq!wq!

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+	int i;
+	for (i = 1; i < 101; i = i+1) {
+
+	    if (i % 15 == 0) {
+	       printf("%d fizzbuzz\n", i);
+            } else if (i % 5 == 0) {
+	       printf("%d buzz\n", i);
+	    } else if (i % 3 == 0) {
+	       printf("%d fizz\n", i);	    
+	    }
+	      else if 
+	       printf("%d\n", i);	
+	}
+}
