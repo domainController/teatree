23db1bc786b50dbd172c8ba6c394dc92ee5e0ba5
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-04 12:42:06 +0000

    /home/neutrino/Fizzbuzz_C/main.c

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 int main ()
 {
 	int i;
-	for (i = 1; i < 101; i = i+1) {
+	for (i = 1; i <= 100 ; i = i+1) {
 
 	    if (i % 15 == 0) {
 	       printf("%d fizzbuzz\n", i);
+
             } else if (i % 5 == 0) {
 	       printf("%d buzz\n", i);
+
 	    } else if (i % 3 == 0) {
 	       printf("%d fizz\n", i);	    
+
 	    }
-	      else if 
+	      else 
 	       printf("%d\n", i);	
 	}
 }
